686bf64d45b8baf7e38224d260f3801ae82584c7
Vlad Melnikov <vlavlamel@gmail.com>
2017-05-24 14:14:39 +0300

    I am too lazy to finish it, but the exercise is clear and not difficult

--- app/src/main/java/com/example/android/emojify/Emojifier.java
+++ app/src/main/java/com/example/android/emojify/Emojifier.java
@@ -29,6 +29,7 @@ import com.google.android.gms.vision.face.FaceDetector;
 class Emojifier {
 
     private static final String LOG_TAG = Emojifier.class.getSimpleName();
+    public int Emoji;
 
     /**
      * Method for detecting faces in a bitmap.
@@ -61,8 +62,12 @@ class Emojifier {
                 Face face = faces.valueAt(i);
 
                 // Log the classification probabilities for each face.
-                getClassifications(face);
-                // TODO (6): Change the call to getClassifications to whichEmoji() to log the appropriate emoji for the facial expression.
+                if (face != null) {
+                    whichEmoji(face);
+                    // TODO (6): Change the call to getClassifications to whichEmoji() to log the appropriate emoji for the facial expression.
+                }else{
+                    Toast.makeText(context, "Make another foto, please бля", Toast.LENGTH_SHORT).show();
+                }
             }
 
         }
@@ -78,14 +83,15 @@ class Emojifier {
      *
      * @param face The face to get the classification probabilities.
      */
-    private static void getClassifications(Face face){
+    private static void whichEmoji(Face face){
         // TODO (2): Change the name of the getClassifications() method to whichEmoji() (also change the log statements)
+        float r = face.getIsRightEyeOpenProbability();
+        float l = face.getIsLeftEyeOpenProbability();
+        float s = face.getIsSmilingProbability();
         // Log all the probabilities
-        Log.d(LOG_TAG, "getClassifications: smilingProb = " + face.getIsSmilingProbability());
-        Log.d(LOG_TAG, "getClassifications: leftEyeOpenProb = "
-                + face.getIsLeftEyeOpenProbability());
-        Log.d(LOG_TAG, "getClassifications: rightEyeOpenProb = "
-                + face.getIsRightEyeOpenProbability());
+        Log.d(LOG_TAG, "getClassifications: smilingProb = " + s);
+        Log.d(LOG_TAG, "getClassifications: leftEyeOpenProb = " + l);
+        Log.d(LOG_TAG, "getClassifications: rightEyeOpenProb = " + r);
 
         // TODO (3): Create threshold constants for a person smiling, and and eye being open by taking pictures of yourself and your friends and noting the logs.
         // TODO (4): Create 3 boolean variables to track the state of the facial expression based on the thresholds you set in the previous step: smiling, left eye closed, right eye closed.
