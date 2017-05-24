503f6fc2ceb1592e9dd0a280480d9257d33535cf
Vlad Melnikov <vlavlamel@gmail.com>
2017-05-24 13:44:53 +0300

    Exercises are done but still working only in landscapre mode

--- app/src/main/java/com/example/android/emojify/Emojifier.java
+++ app/src/main/java/com/example/android/emojify/Emojifier.java
@@ -54,15 +54,33 @@ class Emojifier {
         Log.d(LOG_TAG, "detectFaces: number of faces = " + faces.size());
 
         // If there are no faces detected, show a Toast message
-        if(faces.size() == 0){
+        if (faces.size() == 0) {
             Toast.makeText(context, R.string.no_faces_message, Toast.LENGTH_SHORT).show();
         }
 
         // TODO (2): Iterate through the faces, calling getClassifications() for each face.
+        int flag = 1;
+        for (int i=0; i<faces.size();i++){
+            Log.v(LOG_TAG,"Face- " + flag+". Right eye = "+getClassification(faces.get(i))[0]+
+                    ". Left eye = "+getClassification(faces.get(i))[1]+
+                    ". Smiling = "+getClassification(faces.get(i))[2]);
+            flag ++;
+        }
 
         // Release the detector
         detector.release();
     }
 
     // TODO (1): Create a static method called getClassifications() which logs the probability of each eye being open and that the person is smiling.
+    static float[] getClassification(Face face) {
+        float[] array = new float[3];
+        float r = face.getIsRightEyeOpenProbability();
+        array[0] = r;
+        float l = face.getIsLeftEyeOpenProbability();
+        array[1] = l;
+        float s = face.getIsSmilingProbability();
+        array[2] = s;
+        return array;
+
+    }
 }
